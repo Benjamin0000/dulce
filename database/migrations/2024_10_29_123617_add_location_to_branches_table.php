d7abeded2182753db8f3b1e8d462832af25c8080
Home <you@example.com>
2024-10-31 18:06:03 +0100

    made a change

--- database/migrations/2024_10_29_123617_add_location_to_branches_table.php
+++ database/migrations/2024_10_29_123617_add_location_to_branches_table.php
@@ -13,17 +13,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('branches', function (Blueprint $table) {
-            // Add the new vat column
-            $table->decimal('vat', 65, 3)->default(0); 
-            $table->decimal('cost_per_km', 65, 2)->default(0); 
-            $table->decimal('max_km', 65, 2)->default(0); 
-
             // Add the location column with a default value
             // You can either add the column in this way or separate it into its own statement
             DB::statement('ALTER TABLE branches ADD location POINT NOT NULL DEFAULT ST_GeomFromText(\'POINT(0 0)\')');
 
             // Add the spatial index on the location column
             DB::statement('ALTER TABLE branches ADD SPATIAL INDEX location_spatial_index(location)');
+            // Add the new vat column
+            $table->decimal('vat', 65, 3)->default(0); 
+            $table->decimal('cost_per_km', 65, 2)->default(0); 
+            $table->decimal('max_km', 65, 2)->default(0); 
+
+
         });
     }
 
