e9e9f00007a80cb7f4c6346dcd1fd95b9a5f0dd7
Home <you@example.com>
2024-11-06 17:51:52 +0100

    added the vat_total_to_order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $appends = ['vat_cost'];
+
+    public function getVatCostAttribute()
+    { 
+        $vat = $this->vat; 
+        $total = $this->total;
+        if($discount = $this->discount){
+            $total -= ($discount/100) * $total; 
+        }
+        
+        return $vat ? ($vat/100) * $total : 0; //set vat 
+    }
+    
 }
